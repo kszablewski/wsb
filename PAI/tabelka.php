ffa671e057507e8742e6cc3716c1211bfef0bd5a
kszablewski <61905328+kszablewski@users.noreply.github.com>
2020-03-26 22:02:31 +0100

    Add files via upload

---
+++ PAI/tabelka.php
@@ -0,0 +1,58 @@
+<doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela</title>
+    <style>
+        table, td {
+            border: 1.5px solid black;
+            border-collapse: collapse;
+            width: 50%;
+            height: 50%;
+            text-align: center;
+        }
+        th{
+            border: 0px;
+            border collapse: collapse;
+            height: 20px;
+            padding: 0px;
+          }
+          </style>
+</head>
+<body>
+    <div id="top">
+       
+         <div id="MENU">
+                    <h3 style="font-size:1vw"></h3>
+        </div>
+        
+        <div id="TRESC">
+      
+<table>
+  <tr>
+    <td rowspan="2">1.1</td>
+    <td >1.2</td>
+  </tr>
+  <tr>
+    <td rowspan="2">2.2</td>
+      <th>
+      </th>
+      
+  </tr>
+  <tr>
+    <td>31.</td>
+  </tr>
+  <tr>
+    <td colspan="2">4.1</td>
+  </tr>
+</table>
+        
+        </div>
+        <div id="STOPKA">
+        <h2 style="font-size:0.8vw"></h2>
+        </div>
+    </div>
+</body>
+</html>
+        
